b20a0bb63fe9209f66754a08f8a8588cba83e387
Alan_ <alanwilson@163.com>
2022-11-11 22:47:01 +0800

    test

---
+++ handler.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+	api "github.com/Alan-wW/test/kitex_gen/api"
+)
+
+// HelloImpl implements the last service interface defined in the IDL.
+type HelloImpl struct{}
+
+// Echo implements the HelloImpl interface.
+func (s *HelloImpl) Echo(ctx context.Context, req *api.Request) (resp *api.Response, err error) {
+	// TODO: Your code here...
+	return
+}
